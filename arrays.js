4724c997ff84720d7bae038f867645f451ccfaed
Magnus <mazejumper01@gmail.com>
2025-02-17 10:34:05 +0100

    løst alle opgaver

--- arrays.js
+++ arrays.js
@@ -6,4 +6,51 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // --- Løs opgaverne herunder --- 
 
+    // -- Opgave 1 --
+    let newNumbers = numbersArray.filter(function(number) {
+        return number  > 20;
+    })
+
+    // -- opgave 2 --
+    catsArray.push("Sniffles");
+
+    // -- opgave 3 --
+
+    let hasTrixie = dogsArray.includes("Trixie")
+
+    let hasBaxter = dogsArray.some((dog) => dog == "Baxter")
+
+    // -- opgave 4 --
+
+    let hasBagheera = catsArray.find((cat => cat == "Bagheera"))
+
+    let hasPumpkin = catsArray.find(cat => cat == "Pumpkin")
+
+    // -- opgave 5 --
+
+    let mulitplied = numbersArray.map((number) => number * 3)
+    
+    // -- opgave 6 --
+
+    let dogsString = dogsArray.join("")
+
+    // -- opgave 7 --
+
+    let mangoIndex = fruitsArray.indexOf("Mango")
+
+    let blueberryIndex = fruitsArray.indexOf("Blåbær")
+
+    // -- opgave 8 --
+
+
+    // let deleteIndex = dogsArray.indexOf("Hans")
+    // if(deleteIndex > -1) dogsArray.splice(deleteIndex, 1)
+
+    let dogsModified = dogsArray.filter(dog => dog != "Polly")
+
+    // -- ekstra opgave --
+
+    let sum = numbersArray.reduce( (result, number) => result + number, 500 )
+
+
 }) // ends DOMContentLoaded
